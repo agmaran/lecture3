a7b7318cc08c365794b9dea745b45b3b6d6aff64
ArantxaGarciaMoreno <arantxagarciamoreno@gmail.com>
2021-01-30 07:13:53 -0400

    first commit

---
+++ hello/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, "hello/index.html")
+
+
+def arantxa(request):
+    return HttpResponse("Hello, Arantxa!")
+
+
+def david(request):
+    return HttpResponse("Hello, David!")
+
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
