30c91b9af20e7e296ba3defee6205c8fea9f2b24
ArantxaGarciaMoreno <arantxagarciamoreno@gmail.com>
2021-01-31 07:18:12 -0400

    tasks

--- hello/urls.py
+++ hello/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+app_name = "hello"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:name>", views.greet, name="greet"),
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,37 @@
+from django import forms
 from django.shortcuts import render
-tasks = ["foo", "bar", "baz"]
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+
 # Create your views here.
 
 
 def index(request):
+    if "tasks" not in request.session:  # Is it already a list of tasks in this session
+        # If there is not a list of tasks, I create it as an empty list
+        request.session["tasks"] = []
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
+    })
+
+
+def add(request):
+    if request.method == "POST":  # Checks if the request method is POST
+        # We take all the data submitted and save it in the form variable
+        form = NewTaskForm(request.POST)
+        if form.is_valid():  # We check if the data submitted is valid
+            task = form.cleaned_data["task"]  # We get the task from the form
+            # We add the task to the list of tasks
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:  # If the form is not valid
+            return render(request, "tasks/add.html", {  # We render the add.html file
+                "form": form  # We pass in the form they submitted so they can see the error in the data submitted
+            })
+    return render(request, "tasks/add.html", {  # If the request method is not POST we just render an empty form
+        "form": NewTaskForm()
     })
